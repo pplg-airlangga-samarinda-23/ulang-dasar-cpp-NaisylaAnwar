5e4540d4aad9098b3a61e9513bc103cd97730a42
NaisylaAnwar <23_naisylanwar@student.smkti.net>
2024-07-31 11:13:08 +0700

    selesai

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
+void CetakData(string nama, string nisn, string jurusan, int tinggiBadan)
+{
+    cout << "nama anda: " << nama << endl;
+    cout << "nisn anda: " << nisn << endl;
+    cout << "jurusan anda: " << jurusan << endl;
+    cout << "tinggi badan anda: " << tinggiBadan << endl;
+}
+
+int main()
+{
+    string nama = "Naisyla Anwar";
+    string nisn = "0089039196";
+    string jurusan = "PPLG";
+    int tinggiBadan = 162;
+
+    CetakData(nama, nisn, jurusan, tinggiBadan);
     return 0;
 }
